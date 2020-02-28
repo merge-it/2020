d97424f2237ec1fc9a9266e06715f3d8b387f07a
Roberto Guido <bob4job@gmail.com>
2020-02-28 17:30:13 +0100

    fix panel pa

--- sito/softwarelibero_pubblica_amministrazione.php
+++ sito/softwarelibero_pubblica_amministrazione.php
@@ -14,7 +14,7 @@ page([
         "Per discutere e ridiscutere il Codice di Amministrazione Digitale, le Linee Guida pubblicate da AgID, il concetto di riuso, il ruolo della community e quello dell'impresa, le opportunità di risparmio, i vincoli e i limiti normativi.",
         "IL PANEL È PROVVISORIO E IN FASE DI DEFINIZIONE. Se anche il tuo ente vuole partecipare, per presentare la propria esperienza con il software libero e le proprie soluzioni open source, <a href=\"#contacts\">non esitare a contattarci</a>.",
         "Interverranno:",
-        "<ul><li>Città di Torino</li><li>Agenzia per l'Italia Digitale</li><li>ARPA Piemonte</li><li>Regione Emilia Romagna</li><li>Corpo Nazionale dei Vigili del Fuoco</li><li>Master in Management del Software Libero</li><li>...</li></ul>"
+        "<ul><li>Città di Torino</li><li>Agenzia per l'Italia Digitale</li><li>ARPA Piemonte</li><li>Regione Emilia Romagna</li><li>PloneGov</li><li>Corpo Nazionale dei Vigili del Fuoco</li><li>Master in Management del Software Libero</li><li>CSI Piemonte</li><li>...</li></ul>"
     ],
     'register' => 'optional'
 ]);
